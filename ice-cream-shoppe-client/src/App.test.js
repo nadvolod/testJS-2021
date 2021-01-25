80213fcb620aa4d86ceba7efe6ef3aedd0bcf0c7
nadvolod <nadvolod@gmail.com>
2021-01-25 12:54:00 -0500

    write failing test

--- ice-cream-shoppe-client/src/App.test.js
+++ ice-cream-shoppe-client/src/App.test.js
@@ -1,4 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test.todo("some tests here");
+test("displays images from the server", async () => {
+    render(<App />);
+    //saying that we want all images that end in alt text of `flavor` that are case insensitive
+    const images = await screen.findAllByRole("img", { name: /flavor$/i});
+    expect(images).toHaveLength(2);
+});
